8999aa33d4f1105757b3198235952c0c83987ef1
Bipul Lamsal <bipullamsal@gmail.com>
2026-03-13 21:43:23 +0545

    test: updated parser.lua

--- test/modules/lua/playlist/parser.lua
+++ test/modules/lua/playlist/parser.lua
@@ -2,6 +2,8 @@
 function probe()
 	vlc.msg.dbg("Initiated probe call")
 	vlc.msg.dbg("Access: " .. vlc.access .. " Path:" .. vlc.path)
+	local value = vlc.peek(10000000000000, 20)
+	vlc.msg.dbg("Peeked value " .. value)
 	return (vlc.access == "file")
 end
 
